ad526221e5f002349f15699b6f09a5cbd20178aa
Ravan33-120 <ravannabiyev466@gmail.com>
2025-11-23 19:00:47 +0000

    square 0

---
+++ python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
+class Square:
+    """Class that defines a square."""
+
+    def __init__(self, size):
+        """Initialize the square with a private size attribute."""
+        self.__size = size
